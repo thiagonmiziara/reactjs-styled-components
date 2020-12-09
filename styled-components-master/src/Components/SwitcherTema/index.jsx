a80fe1ba3c0665d9a910f8e8938dd53f759965a3
thiagonmiziara <thiagonmiziara@gmail.com>
2020-12-09 19:59:02 -0300

    projeto finalizado

---
+++ styled-components-master/src/Components/SwitcherTema/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ThemeOn from '../../assets/images/themeOff.svg';
+import ThemeOff from '../../assets/images/themeOn.svg';
+import {Icone} from '../UI';
+
+const claro = <Icone src={ThemeOn} alt="Tema Claro"/>
+const escuro =<Icone src={ThemeOff} alt="Tema Escuro"/>
+
+export default ({tema})=>{
+ return(
+   tema ? escuro : claro
+ )
+}
+
+
